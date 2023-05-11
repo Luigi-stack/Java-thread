9f14cc49d486feb7b11ef15e663340c1c141fd72
Luigi Iossa <luigi_iossa@hotmail.com>
2023-05-11 07:49:56 +0000

    commit

--- java-threads/src/esercizio3/MainProjct.java
+++ java-threads/src/esercizio3/MainProjct.java
@@ -7,27 +7,28 @@ import org.apache.commons.io.FileUtils;
 public class MainProjct {
 
 	public static void main(String[] args) throws IOException {
+		scriviPresenze();
 	}
 	
 	public static void scriviPresenze() throws IOException {
         String presenze = "";
-        File file = new File("Registro /test.txt");
+        File file = new File("registro/test.txt");
         FileUtils.writeStringToFile(file, presenze, "UTF-8"); 
     }
 
     public static void leggiRegistro() throws IOException {
-        File file = new File("Registro/test.txt");
-        String read = FileUtils.readFileToString(file, "UFT-8");
+        File file = new File("registro/test.txt");
+        String read = FileUtils.readFileToString(file, "UTF-8");
         System.out.println(read);
 
     }
 
-    public static void cancellaRegistro() throws IOException {
-        File file = new File("Registro/");
-
-        FileUtils.deleteDirectory(file);
-
-    }
+//    public static void cancellaRegistro() throws IOException {
+//        File file = new File("registro/");
+//
+//        FileUtils.deleteDirectory(file);
+//
+//    }
 
 
 }
